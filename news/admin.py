06a4fd7b8fdb5d00ea93f80695f707e68cdecc53
43 <43@mail.ru>
2023-10-07 10:43:31 +0500

    Поиск по триграммам, начало

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.postgres.search import SearchVector, TrigramSimilarity
 
 from news.models import News
 
@@ -15,7 +16,26 @@ class NewsAdmin(admin.ModelAdmin):
                     'hidden']
     search_fields = ['title',
                      'text',
-                     'author']
+                     'author__username',
+                     'author__first_name',
+                     'author__last_name']
     list_filter = ['publish_date',
                    'edit_date',
                    'hidden']
+
+    def get_search_results(self, request, queryset, search_term: str):
+        if not search_term.strip():
+            return queryset, False
+
+        queryset = queryset.annotate(
+            similarity=TrigramSimilarity('title', search_term)
+        ).filter(similarity__gt=0.03).order_by('-similarity')
+
+        parent_queryset, may_have_duplicates = super()\
+            .get_search_results(request, queryset, search_term)
+
+        result_queryset = queryset | parent_queryset
+        result_queryset = result_queryset.distinct()
+
+        return result_queryset, False
+
