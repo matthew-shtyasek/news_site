3822bdf87e9d1d47585714c92e83b6acc575a044
43 <43@mail.ru>
2023-10-07 08:46:35 +0500

    model created

---
+++ news/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=128,
+                             verbose_name='Заголовок')
+    text = models.TextField(blank=False,
+                            verbose_name='Текст')
+    publish_date = models.DateTimeField(auto_now_add=True,
+                                        verbose_name='Дата публикации')
+    edit_date = models.DateTimeField(auto_now=True,
+                                     verbose_name='Дата редактирования')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               verbose_name='Автор')
