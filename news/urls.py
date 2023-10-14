6f5557ec8b027fa19c7055c71bb487ec903289f0
43 <43@mail.ru>
2023-10-14 13:16:07 +0500

    Добавили news:details

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
+from django.views.generic import DetailView
 
+from news.models import News
 from news.views import NewsListView
 
 app_name = 'news'
 
 urlpatterns = [
-    path('', NewsListView.as_view())
+    path('', NewsListView.as_view()),
+    path('<int:pk>/',
+         DetailView.as_view(model=News,
+                            template_name='news/news_details.html',
+                            context_object_name='news'),
+         name='details')
 ]
